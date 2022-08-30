36e93f715368563badd7c5ebe1592357d6857de3
ko7ko <NLs6houh@protonmail.com>
2022-08-31 03:03:36 +0400

    update port

--- src/app.js
+++ src/app.js
@@ -71,6 +71,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(process.env.PORT, () => {
+    console.log(`Server is up on port ${process.env.PORT}.`)
 })
